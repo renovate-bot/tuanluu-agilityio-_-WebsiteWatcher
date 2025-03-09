4f27e71e3f48beca464113e7ca59869b4670bb3d
Tuan Luu <tuan.luu@asnet.com.vn>
2025-03-09 22:19:02 +0700

    Implementing the SqlOutput binding in the Snapshot function

--- Snapshot.cs
+++ Snapshot.cs
@@ -1,3 +1,4 @@
+using HtmlAgilityPack;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Extensions.Sql;
 using Microsoft.Extensions.Logging;
@@ -7,13 +8,32 @@ namespace WebsiteWatcher;
 public class Snapshot(ILogger<Snapshot> logger)
 {
     [Function(nameof(Snapshot))]
-    public void Run(
+    [SqlOutput("dbo.Snapshots", "WebsiteWatcher")]
+    public SnapshotRecord? Run(
             [SqlTrigger("[dbo].[Websites]", "WebsiteWatcher")] IReadOnlyList<SqlChange<Website>> changes)
     {
         foreach (var change in changes)
         {
             logger.LogInformation($"Change Type: {change.Operation}");
             logger.LogInformation($"Website Id: {change.Item.Id} Website Url: {change.Item.Url}");
+
+            if (change.Operation != SqlChangeOperation.Insert)
+            {
+                continue;
+            }
+
+            HtmlWeb web = new();
+            HtmlDocument doc = web.Load(change.Item.Url);
+            var divWithContent = doc.DocumentNode.SelectSingleNode(change.Item.XPathExpression);
+            var content = divWithContent != null ? divWithContent.InnerText.Trim() : "No content";
+
+            logger.LogInformation($"Content: {content}");
+
+            return new SnapshotRecord(change.Item.Id, content);
         }
+
+        return null;
     }
 }
+
+public record SnapshotRecord(Guid Id, string Content);
